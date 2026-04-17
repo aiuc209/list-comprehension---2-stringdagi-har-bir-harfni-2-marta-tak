483b0224b969a5d3d7fac9bcc185b564fb4cb24c
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:28:46 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+List Comprehension   2 Stringdagi Har Bir Harfni 2 Marta Tak
+Topic: LIST COMPREHENSION - 2: �� Stringdagi har bir harfni 2 marta takrorlab yangi ro‘yxat tuzing Masalan: "salom" → ['ss', 'aa', 'll', 'oo', 'mm']
+"""
+
+def main():
+    print("Welcome to List Comprehension   2 Stringdagi Har Bir Harfni 2 Marta Tak!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
